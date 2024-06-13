878402c7e31cd77f31d2648317d5745ac5029988
Ari Alberto <154787080+AriRJ@users.noreply.github.com>
2024-06-13 05:52:20 -0300

    Update Curso.java

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,18 +1,23 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso extends Conteudo{
-
+public class Curso extends Conteudo {
     private int cargaHoraria;
+
+    public Curso() {
+    }
+
+    public Curso(String titulo, String descricao, int cargaHoraria) {
+        this.setTitulo(titulo);
+        this.setDescricao(descricao);
+        this.cargaHoraria = cargaHoraria;
+    }
 
     @Override
     public double calcularXp() {
         return XP_PADRAO * cargaHoraria;
     }
 
-    public Curso() {
-    }
-
-
+    // Getters and Setters
     public int getCargaHoraria() {
         return cargaHoraria;
     }
@@ -30,3 +35,4 @@ public class Curso extends Conteudo{
                 '}';
     }
 }
+
